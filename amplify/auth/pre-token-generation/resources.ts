5c5f568d714753317e9d327931598ad749a27eb5
EC2 Default User <ec2-user@ip-172-31-38-82.ec2.internal>
2024-07-12 18:59:53 +0000

    initial import

--- amplify/auth/pre-token-generation/resources.ts
+++
@@ -1,8 +0,0 @@
-import { defineFunction } from '@aws-amplify/backend';
-
-//https://aws.amazon.com/blogs/security/how-to-customize-access-tokens-in-amazon-cognito-user-pools/
-//https://docs.amplify.aws/vue/build-a-backend/functions/examples/override-token/
-export const preTokenGeneration = defineFunction({
-  name: 'pre-token-generation'
-});
-
